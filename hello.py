a09a01db3a3551382036e70dc8629ba307569b51
719098 <113333214+719098@users.noreply.github.com>
2025-03-13 11:42:08 +0800

    pythonweb網頁

---
+++ hello.py
@@ -0,0 +1,14 @@
+x = int(input("請輸入一個正整數："))
+def square(y):
+  print("{} 的平方為 {}".format(y, y*y)) 
+
+if (x<=0):
+	print("您輸入的值小於等於0")
+else:
+	for i in range(x):
+		square(i)
+def mis2a():
+    print("資管二A")
+
+def mis2b():
+    print("資管二B")
